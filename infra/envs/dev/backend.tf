6d1ea6982c88b0221371c16e0a509fcdd8f13627
J-Soriano <jsoriano.tech@gmail.com>
2026-02-16 15:12:57 -0500

    Add Terraform backend config for dev

---
+++ infra/envs/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloudops-tfstate-rg"
+    storage_account_name = "cloudopstfstate01"   # <-- replace with YOUR storage account name
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
